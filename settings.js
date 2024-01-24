706aef3e8696ad63f2274f488e642e891335e72f
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-01-25 00:23:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://ajiart01:Ajifu917@cluster0.zebgfie.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "ajiart01@gmail.com" //email
+global.email_password = "ptdn ppxh ethp ncgi" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
